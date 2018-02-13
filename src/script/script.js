bdbd5a9dc2f407ea143e02b60f1d636f6873f125
Yurii <gawrelyuk@gmail.com>
2018-02-13 02:16:26 +0200

    added comments section and footer

--- src/script/script.js
+++ src/script/script.js
@@ -5,4 +5,17 @@ $(document).ready(function() {
 		$(".js-header-hand").animate({
 			'right': '0'
 		}, 1000);
+
+
+
+//functionn show comments
+		$(".comment__face").hover(function(e){
+			var $this = $(e.currentTarget);
+			var text =  $this.data('text');
+			var name =  $this.data('name');
+	
+			$(".js-comment-text").html("“" + text + "”");
+			$(".js-comment-name").html(name);
+		});
+
 });
